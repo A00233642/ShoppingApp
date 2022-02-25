ebe488eaffce366235cf85d878669230ea4d4dd3
A00233642 <Taiwoakintunde26@gmail.com>
2022-02-24 22:48:52 -0500

    UserService
    I created a new class under Service folder named it UserService. i define variable for firebase client, create an instance for the firebase client, define method to support login and register functionalities

---
+++ ShoppingApp/Views/ProductsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShoppingApp.Views
+{
+    public partial class ProductsView : ContentPage
+    {
+        public ProductsView()
+        {
+            InitializeComponent();
+        }
+
+       async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+          await  Navigation.PopModalAsync();
+        }
+    }
+}
