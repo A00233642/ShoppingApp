bc3616fe3c4cf69eb2bb91ad5629070b54072b66
A00233642 <Taiwoakintunde26@gmail.com>
2022-02-24 23:05:19 -0500

    Assemblyinfo.cs
    I added  exported webfont to my application

--- ShoppingApp/AssemblyInfo.cs
+++ ShoppingApp/AssemblyInfo.cs
@@ -1,3 +1,9 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("CabinSketch-Bold.ttf", Alias = "CabinSketch")]
+
+
+[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "FontAwesome")]
