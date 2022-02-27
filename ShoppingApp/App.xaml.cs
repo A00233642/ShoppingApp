183dcf5c5ffb15f2142ba57c3156755b8dede23a
A00233642 <Taiwoakintunde26@gmail.com>
2022-02-27 02:38:19 -0500

    App.xaml
    Making the settings page my first page in order to mange categories and products

--- ShoppingApp/App.xaml.cs
+++ ShoppingApp/App.xaml.cs
@@ -12,7 +12,8 @@ namespace ShoppingApp
             InitializeComponent();
 
             // MainPage = new MainPage();
-            MainPage = new LoginView();
+            //MainPage = new LoginView();
+            MainPage = new NavigationPage(new SettingsPage());
         }
 
         protected override void OnStart()
