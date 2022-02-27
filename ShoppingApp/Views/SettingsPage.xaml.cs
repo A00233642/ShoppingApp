9c42fa672ed4cd5ad81b7b45afd23b59d7dc803f
A00233642 <Taiwoakintunde26@gmail.com>
2022-02-27 02:40:33 -0500

    SettingsPage
    within the views folder i added a new file named it SettingsPage, i added label and button to manage categories and products and also an event handler for my button

---
+++ ShoppingApp/Views/SettingsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ShoppingApp.Helpers;
+using Xamarin.Forms;
+
+namespace ShoppingApp.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+       async void ButtonCategories_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            var acd = new AddCategoryData();
+            await acd.AddCategoriesAsync();
+
+        }
+
+       async void ButtonProducts_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var afd = new AddShopItemData();
+            await afd.AddShopItemAsync();
+        }
+    }
+}
