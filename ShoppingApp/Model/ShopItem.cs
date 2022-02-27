4553e69717eaeec1d7d682e4307074e292522173
A00233642 <Taiwoakintunde26@gmail.com>
2022-02-27 02:47:52 -0500

    ShopItem.cs
    Within the helpers folder i added a new file named it ShopItem.cs, then i defined the properties required for shopItem

---
+++ ShoppingApp/Model/ShopItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ShoppingApp.Model
+{
+    public class ShopItem
+    {
+        public int ProductID { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public string Rating { get; set; }
+        public string RatingDetail { get; set; }
+        public string HomeSelected { get; set; }
+        public object MyProperty { get; set; }
+        public int CategoryID { get; set; }
+        public int Price { get; set; }
+    }
+}
