0f2d931993c9a66053adcc3195b7b0e36fce0081
A00233642 <Taiwoakintunde26@gmail.com>
2022-03-17 14:01:38 -0400

    ProductsDetailsView.xaml
    User interface layout for product details view details of any selected item and also an imagebutton for selection

---
+++ ShoppingApp/Views/ProductDetailsView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using ShoppingApp.Model;
+using ShoppingApp.ViewModels;
+using Xamarin.Forms;
+
+namespace ShoppingApp.Views
+{
+    public partial class ProductDetailsView : ContentPage
+    {
+        ProductDetailsViewModel pvm;
+        public ProductDetailsView(ShopItem shopItem)
+        {
+            InitializeComponent();
+            pvm = new ProductDetailsViewModel(shopItem);
+            this.BindingContext = pvm;
+        }
+
+        async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
