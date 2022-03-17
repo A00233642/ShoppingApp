29c7e683b09d515d26160b438c89b10525b75e3d
A00233642 <Taiwoakintunde26@gmail.com>
2022-03-17 13:58:39 -0400

    LogoutView.xaml.cs
    Imagebutton click to logout

--- ShoppingApp/Views/LogoutView.xaml.cs
+++ ShoppingApp/Views/LogoutView.xaml.cs
@@ -11,5 +11,10 @@ namespace ShoppingApp.Views
         {
             InitializeComponent();
         }
+
+        async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
     }
 }
